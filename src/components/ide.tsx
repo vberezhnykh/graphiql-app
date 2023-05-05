9f2a314a721e4a310541ed31db52094fc44c2ebd
janChorny <janchorny@tut.by>
2023-05-05 23:30:37 +0300

    feat: add basic main page layout

---
+++ src/components/ide.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const IDE = () => {
+  return (
+    <div className="editor">
+      <div className="editor__container">
+        <div className="editor__docs">
+          <h4 className="editor__header docs-header">Docs</h4>
+        </div>
+        <div className="editor__request">
+          <h4 className="editor__header request-header">Request</h4>
+          <div className="editor__request-container">
+            <div className="editor__headers">
+              <h5 className="editor__request-inner-header">Headers</h5>
+              <div className="editor__request-inner-body">Headers body</div>
+            </div>
+            <div className="editor__variables">
+              <h5 className="editor__request-inner-header">Variables</h5>
+              <div className="editor__request-inner-body">Variables body</div>
+            </div>
+          </div>
+          <button className="editor__request-button">Request</button>
+        </div>
+        <div className="editor__response">
+          <h4 className="editor__header response-header">Response</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IDE;
--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,10 +1,13 @@
+import IDE from '../components/ide';
 import Header from '../components/header';
 
 const MainPage = () => {
   return (
     <>
       <Header />
-      <main className="main--flex main">This is Main Page</main>
+      <main className="main--flex main">
+        <IDE />
+      </main>
       <footer>Footer Template</footer>
     </>
   );
