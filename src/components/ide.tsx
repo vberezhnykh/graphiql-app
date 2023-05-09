ca2c568230d50a9fd99e36decd555d979ed9c914
janChorny <janchorny@tut.by>
2023-05-09 20:34:06 +0300

    feat: add basic styles to continers

--- src/components/ide.tsx
+++ src/components/ide.tsx
@@ -9,6 +9,7 @@ const IDE = () => {
         </div>
         <div className="editor__request">
           <h4 className="editor__header request-header">Request</h4>
+          <textarea className="editor__textarea-request" name="" id=""></textarea>
           <div className="editor__request-container">
             <div className="editor__headers">
               <h5 className="editor__request-inner-header">Headers</h5>
@@ -23,6 +24,7 @@ const IDE = () => {
         </div>
         <div className="editor__response">
           <h4 className="editor__header response-header">Response</h4>
+          <textarea className="editor__textarea-response" name="" id=""></textarea>
         </div>
       </div>
     </div>
