4ebe136d08370ac7cb4ca0bff4d945543c535099
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-05-10 20:40:02 +0300

    refactor: remove unnecessary login input on register form

--- src/components/registerForm.tsx
+++ src/components/registerForm.tsx
@@ -22,7 +22,6 @@ const RegisterForm = () => {
   };
 
   const nameRegex = /^[A-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
-  const loginRegex = /^[A-Za-z0-9]+([A-Za-z0-9]*|[._-]?[A-Za-z0-9]+)*$/g;
   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
   const emailRegex = /\S+@\S+\.\S+/;
 
@@ -31,56 +30,29 @@ const RegisterForm = () => {
       <h1>Sign up</h1>
       <ul className="errors-list">
         <ErrorMessage errors={errors} name="name" as="li" />
-        <ErrorMessage errors={errors} name="login" as="li" />
         <ErrorMessage errors={errors} name="email" as="li" />
         <ErrorMessage errors={errors} name="password" as="li" />
       </ul>
       <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-field-group">
-          <fieldset className="form-field user__name">
-            <label htmlFor="user_name">Name</label>{' '}
-            <input
-              type="text"
-              id="user_name"
-              className={`text-input ${errors.name ? 'text-input--error' : undefined}`}
-              {...register('name', {
-                pattern: {
-                  value: nameRegex,
-                  message: 'Name is invalid',
-                },
-                minLength: {
-                  value: 3,
-                  message: 'Name must consist of at least 2 letters',
-                },
-                required: 'Name cannot be empty',
-              })}
-            />
-          </fieldset>
-          <fieldset className="form-field user__login">
-            <label htmlFor="user_login">Username</label>
-            <input
-              type="text"
-              id="user_login"
-              className={`text-input ${errors.login ? 'text-input--error' : undefined}`}
-              {...register('login', {
-                required: 'Login cannot be empty',
-                pattern: {
-                  value: loginRegex,
-                  message:
-                    'Login should only contain alphanumeric characters and/or underscores (_)',
-                },
-                minLength: {
-                  value: 5,
-                  message: 'The number of characters must be between 5 and 15',
-                },
-                maxLength: {
-                  value: 15,
-                  message: 'The number of characters must be between 5 and 15',
-                },
-              })}
-            />
-          </fieldset>
-        </div>
+        <fieldset className="form-field user__name">
+          <label htmlFor="user_name">Name</label>{' '}
+          <input
+            type="text"
+            id="user_name"
+            className={`text-input ${errors.name ? 'text-input--error' : undefined}`}
+            {...register('name', {
+              pattern: {
+                value: nameRegex,
+                message: 'Name is invalid',
+              },
+              minLength: {
+                value: 3,
+                message: 'Name must consist of at least 2 letters',
+              },
+              required: 'Name cannot be empty',
+            })}
+          />
+        </fieldset>
         <fieldset className="form-field user__email">
           <label htmlFor="user_email">Email</label>
           <input
