f46c9e650a889217537cd0e7ed884ea84e2f60d0
janChorny <janchorny@tut.by>
2023-05-18 23:44:53 +0300

    feat: add schema response interfaces and schema transfer to docs components

--- src/components/docs.tsx
+++ src/components/docs.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-import { DocsRenderInterface } from '../utils/interfaces';
+import { DocsRenderInterface, SchemaResponseInterface } from '../utils/interfaces';
 
 function Docs({ rend, text }: DocsRenderInterface) {
   const message = text;
   const show = rend;
+  const mail: SchemaResponseInterface = message ? JSON.parse(message) : '{}';
 
-  return <>{show && <div>{message}</div>}</>;
+  return (
+    <>
+      {show && message && (
+        <div>
+          <div>{mail.data.__schema.queryType.name}</div>
+          <div>{JSON.stringify(mail.data.__schema.types[0])}</div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Docs;
